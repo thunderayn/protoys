f7c00aea5ae581092a0983da3a34597d8cfaab20
Your Name <tangshirong477@gmail.com>
2026-05-30 00:51:27 +0800

    vercel margin

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { StyledEngineProvider } from '@mui/material/styles'
 import { LanguageProvider } from './i18n/LanguageContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StyledEngineProvider injectFirst>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </StyledEngineProvider>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 )
