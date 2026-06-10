4e89d2a99ce16f0353c18219391a3bee8afd7ebf
Your Name <tangshirong477@gmail.com>
2026-06-10 22:56:26 +0800

    toys updated

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+import products1 from './products.json'
+import productsJoybear from './products-joybear.json'
+import productsJoybear2 from './products-joybear2.json'
+
+export const allProducts = [...products1, ...productsJoybear, ...productsJoybear2]
